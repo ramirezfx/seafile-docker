08d4aa4e4a66574a44c3a69f8dd392a3b23b72b4
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-11-30 09:13:42 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo mkdir /usr/share/icons/docker
+sudo cp resources/startkmymoney.sh /usr/local/bin
+sudo cp resources/kmymoney-docker.desktop /usr/share/applications
+sudo cp resources/kmymoney.jpg /usr/share/icons/docker
